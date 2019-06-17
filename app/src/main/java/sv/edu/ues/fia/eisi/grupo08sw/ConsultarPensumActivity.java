a08a4f0ecf2a2ec287a9d0319b1320c8408ad9a5
vanne <ph12010@ues.edu.sv>
2019-06-16 18:21:49 -0600

    Merge branch 'master' of C:\Users\Jenny\Documents\PDM\WebService\Grupo08SW with conflicts.

--- app/src/main/java/sv/edu/ues/fia/eisi/grupo08sw/ConsultarPensumActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo08sw/ConsultarPensumActivity.java
@@ -14,7 +14,7 @@ import sv.edu.ues.fia.eisi.grupo08sw.basedatos.ControlWS;
 @SuppressLint("NewApi")
 public class ConsultarPensumActivity extends AppCompatActivity {
 
-    EditText editIdPensum;
+    EditText editIdPesum;
     TextView txtJson;
 
     @SuppressLint("NewApi")
@@ -25,24 +25,26 @@ public class ConsultarPensumActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        editIdPensum = findViewById(R.id.editIdPensum);
+        editIdPesum = findViewById(R.id.editIdPesum);
         txtJson = findViewById(R.id.consultarJson);
     }
 
     public void consultarPensumServidor(View v) {
         txtJson.setText("");
-        String url = "http://eisi.fia.ues.edu.sv/GPO08/HC14030/ws_pensum_query.php?" +
-                "idPensum=" + editIdPensum.getText().toString();
+        String url = "http://eisi.fia.ues.edu.sv/GPO08/HC14030/ws_pensum_query.php?"+
+                "idPesum="+editIdPesum.getText().toString();
         String json = ControlWS.consultarPensum(url, this);
 
         if (json == null) {
             txtJson.setText("No existe");
         } else {
-            String[] datosJson = json.split(",");
-            String[] id, nombre;
-            id = datosJson[0].split(":");
+           String[]datosJson = json.split(",");
+           String[] id, nombre;
+           id=datosJson[0].split(":");
+           nombre=datosJson[1].split(":");
 
-            txtJson.setText(txtJson.getText().toString() + "id= " + id[1] + "\n");
+           txtJson.setText(txtJson.getText().toString()+"id= "+id[1]+"\n");
+           txtJson.setText(txtJson.getText().toString()+"nombre= "+nombre[1]+"\n");
 
         }
     }
